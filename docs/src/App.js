0776942b5f3901b1133db81e9b963a1aa25f6843
Doug March <dougmarch@gmail.com>
2017-10-17 16:31:59 -0400

    using docs/app for some notes

---
+++ docs/src/App.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const App = props => {
+  return (
+    <div>
+      <h1>Todo</h1>
+
+      <h2>Organization</h2>
+
+      <h3>Atoms</h3>
+      <hr />
+
+      <h3>Molecules</h3>
+      <hr />
+
+      <h3>Organisms</h3>
+      <hr />
+
+      <h3>Templates</h3>
+      <hr />
+
+      <h3>Pages</h3>
+      <hr />
+
+
+      <h2>Components to build</h2>
+      - [ ] Button
+      - [ ] ButtonOutline
+      - [ ] ButtonCircle
+      - [ ] ButtonTransparent
+      - [ ] Link
+      - [ ] NavLink
+      - [ ] BlockLink
+      - [ ] Heading
+      - [ ] Subhead
+      - [ ] Text
+      - [ ] Small
+      - [ ] Lead
+      - [ ] Blockquote
+      - [ ] Measure
+      - [ ] Truncate
+      - [ ] Label
+      - [ ] Input
+      - [ ] Select
+      - [ ] Select2
+      - [ ] Textarea
+      - [ ] Checkbox
+      - [ ] Radio
+      - [ ] Slider
+      - [ ] Image
+      - [ ] Avatar
+      - [ ] BackgroundImage
+      - [ ] Container
+      - [ ] Divider
+      - [ ] Border
+      - [ ] Media
+      - [ ] Card
+      - [ ] Banner
+      - [ ] Panel
+      - [ ] PanelHeader
+      - [ ] PanelFooter
+      - [ ] Progress
+      - [ ] Message
+      - [ ] Group
+      - [ ] Toolbar
+      - [ ] Badge
+      - [ ] Circle
+      - [ ] Tabs
+      - [ ] TabItem
+      - [ ] DotButton
+      - [ ] Close
+      - [ ] Relative
+      - [ ] Absolute
+      - [ ] Fixed
+      - [ ] Sticky
+      - [ ] Drawer
+      - [ ] Overlay
+      - [ ] Carousel
+      - [ ] ScrollCarousel
+      - [ ] CarouselSlide
+      - [ ] Tooltip
+      - [ ] Switch
+      - [ ] Arrow
+      - [ ] Star
+      - [ ] Embed
+      - [ ] Donut
+      - [ ] Row
+      - [ ] Column
+
+      ### Tooling
+      - [ ] Hook up some flavor of CI
+    </div>
+  )
+}
+
+export default App
---
+++ docs/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/entry.js',
+
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    alias: {
+      'cl': path.join(__dirname, '../src')
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'raw-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ],
+
+  devServer: {
+    historyApiFallback: true
+  }
+}
